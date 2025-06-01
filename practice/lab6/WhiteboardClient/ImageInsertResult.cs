da16527f1bf804a7e07d9d3ea16ac84a525c32c1
htcisme <23521673@gm.uit.edu.vn>
2025-06-01 12:39:58 +0700

    Lab6 Full adding

---
+++ practice/lab6/WhiteboardClient/ImageInsertResult.cs
@@ -0,0 +1,24 @@
+﻿using System.Drawing;
+using System.Text.Json.Serialization;
+
+namespace WhiteboardClient
+{
+    public class ImageInsertResult
+    {
+        [JsonIgnore]
+        public Point Position { get; set; }
+
+        [JsonIgnore]
+        public Size Size { get; set; }
+
+        public ImageInsertResult()
+        {
+        }
+
+        public ImageInsertResult(Point position, Size size)
+        {
+            Position = position;
+            Size = size;
+        }
+    }
+}
---
+++ practice/lab6/WhiteboardClient/PointConverter.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Drawing;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace WhiteboardClient
+{
+    public class PointConverter : JsonConverter<Point>
+    {
+        public override Point Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (reader.TokenType == JsonTokenType.StartObject)
+            {
+                int x = 0, y = 0;
+                while (reader.Read())
+                {
+                    if (reader.TokenType == JsonTokenType.EndObject)
+                        break;
+
+                    if (reader.TokenType == JsonTokenType.PropertyName)
+                    {
+                        string propertyName = reader.GetString();
+                        reader.Read();
+
+                        switch (propertyName?.ToLower())
+                        {
+                            case "x":
+                                x = reader.GetInt32();
+                                break;
+                            case "y":
+                                y = reader.GetInt32();
+                                break;
+                        }
+                    }
+                }
+                return new Point(x, y);
+            }
+            return new Point(0, 0);
+        }
+
+        public override void Write(Utf8JsonWriter writer, Point value, JsonSerializerOptions options)
+        {
+            writer.WriteStartObject();
+            writer.WriteNumber("x", value.X);
+            writer.WriteNumber("y", value.Y);
+            writer.WriteEndObject();
+        }
+    }
+
+    public class SizeConverter : JsonConverter<Size>
+    {
+        public override Size Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (reader.TokenType == JsonTokenType.StartObject)
+            {
+                int width = 0, height = 0;
+                while (reader.Read())
+                {
+                    if (reader.TokenType == JsonTokenType.EndObject)
+                        break;
+
+                    if (reader.TokenType == JsonTokenType.PropertyName)
+                    {
+                        string propertyName = reader.GetString();
+                        reader.Read();
+
+                        switch (propertyName?.ToLower())
+                        {
+                            case "width":
+                                width = reader.GetInt32();
+                                break;
+                            case "height":
+                                height = reader.GetInt32();
+                                break;
+                        }
+                    }
+                }
+                return new Size(width, height);
+            }
+            return new Size(0, 0);
+        }
+
+        public override void Write(Utf8JsonWriter writer, Size value, JsonSerializerOptions options)
+        {
+            writer.WriteStartObject();
+            writer.WriteNumber("width", value.Width);
+            writer.WriteNumber("height", value.Height);
+            writer.WriteEndObject();
+        }
+    }
+}
